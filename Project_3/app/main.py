6a390abfe5c21861f08b58a8afc9e3dd665c801a
Jake Beard <jake.beard@hotmail.com>
2022-09-23 15:38:49 +0100

    update dgitnore

--- Project_3/app/main.py
+++ Project_3/app/main.py
@@ -1,6 +1,15 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Thumbnail(BaseModel):
+    url: str
+    filename: Optional[str] = None
+
 
 @app.post("/thumbnail")
 def create_thumbnail():
