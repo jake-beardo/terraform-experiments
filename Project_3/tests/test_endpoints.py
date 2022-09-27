ad792ab12daea028045ec62471254849127917b2
Jake Beard <jake.beard@hotmail.com>
2022-09-27 08:50:34 +0100

    new thumbnail maker

--- Project_3/tests/test_endpoints.py
+++ Project_3/tests/test_endpoints.py
@@ -1,16 +1,28 @@
-from fastapi.testclient import TestClient
 from app.main import app
-
-import pytest
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
+
 def test_create_thumbnail():
-    source_url = "sourceurl"
-    response = client.post('/thumbnail', json={'url': source_url})
+    source_url = "https://jpeg.org/images/jpegai-home.jpg"
+    response = client.post("/thumbnail", json={"url": source_url})
     assert response.status_code == 200
     assert response.json() != None
 
     output = response.json()
     assert output["url"] == source_url
     assert output["filename"] != None
+
+    response = client.get(f'/static/{output["filename"]}')
+    assert response.status_code == 200
+
+
+def test_create_thumbnail_exceptions():
+    sourceurl = "some url"
+    response = client.post("/thumbnail", json={"url": sourceurl})
+    assert response.status_code == 400
+
+    sourceurl = "https://www.google.com"
+    response = client.post("/thumbnail", json={"url": sourceurl})
+    assert response.status_code == 400
